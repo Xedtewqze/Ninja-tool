6295d1cb536d973178caa68d485d5dfe8be27e53
Xedtewqze <143133076+Xedtewqze@users.noreply.github.com>
2023-11-01 21:26:59 +0100

    Update Ninja tool.py

--- Ninja tool.py
+++ Ninja tool.py
@@ -1,1 +1,46 @@
 
+import os
+import sys,time
+os.system("clear")
+text = """
+
+
+
+
+                   
+            The Dragon Tool For Noob Hackers '_' :)
+                 By Bo Haydar El Mo3alem
+
+  ________________________________________________________________
+ |Youtube:https://youtube.com/@BO_HAYDAR_HK  without mistakes √   |
+ |Tele:https://t.me/txrtm                    Easy for Noobs √     |
+ |Tele Team:https://t.me/+EHo65POl1yA2ZjA0   All Hacking Tools √  |
+ |________________________________________________________________|
+
+              Name Tool:Dragon-Tool     Version: 1.4
+              by:BO HAYDAR              Team: ARB TXR TEAM
+
+       _____________________________________________________
+      |_____________________________________________________|
+      |                                                     |
+      |           [1]      ==>     Android Virus            |
+      |           [2]      ==>     Google Cards             |
+      |           [3]      ==>     Hacking Tools            |
+      |           [4]      ==>     Masked Links             |
+      |           [5]      ==>     DDoS Attack              |
+      |           [6]      ==>     Metasploite Fix          |
+      |           [7]      ==>     Peshing Website          |
+      |           [8]      ==>     Generator Payload        |
+      |           [9]      ==>     Ip Info                  |
+      |           [10]     ==>     Ip website               |
+      |           [11]     ==>     Install YouTube video    |
+      |           [12]     ==>     Scan Website & IP        |
+      |           [13]     ==>     Cam Hackers              |
+      |           [14]     ==>     Ngrok Install            |
+      |           [15]     ==>     Ngrok Ports              |
+      |           [16]     ==>     Instgram Info            |
+      |           [00]     ==>     Exit The Tool            |
+      |_____________________________________________________|
+      |_____________________________________________________|
+"""
+os.system(f'echo "{text}" | lolcat')
