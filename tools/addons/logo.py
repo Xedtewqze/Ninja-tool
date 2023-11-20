f57e7b7f233535118648ecef4dd32edbe96b322e
Xedtewqze <143133076+Xedtewqze@users.noreply.github.com>
2023-11-20 23:29:40 +0100

    Update logo.py

--- tools/addons/logo.py
+++ tools/addons/logo.py
@@ -1,26 +1,6 @@
 
 import os
-from colorama import Fore as F
-
-
-def show_logo() -> None:
-    """Print the application logo.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    logo = """
-   _______  .__            __         ________  ________          _________
-   \      \ |__| ____     |__|____    \______ \ \______ \   ____ /   _____/
-   /   |   \|  |/    \    |  \__  \    |    |  \ |    |  \ /  _ \\_____  \ 
-  /    |    \  |   |  \   |  |/ __ \_  |    `   \|    `   (  <_> )        \
-  \____|__  /__|___|  /\__|  (____  / /_______  /_______  /\____/_______  /
-          \/        \/\______|    \/          \/        \/              \/ 
-  """
-
+from colorama import Fore as f 
 
     print(f"{F.RED}{logo}")
     print("├─── DOS TOOL")
